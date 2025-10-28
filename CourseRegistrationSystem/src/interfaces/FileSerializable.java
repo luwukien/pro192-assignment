b24c97a458663458557dd017305b746e016c7fb8
pain_ <handstand30s@gmail.com>
2025-10-28 13:49:09 +0700

    fix FileSerializable and add data models

--- CourseRegistrationSystem/src/interfaces/FileSerializable.java
+++ CourseRegistrationSystem/src/interfaces/FileSerializable.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package interfaces;
 
@@ -8,6 +8,6 @@ package interfaces;
  *
  * @author admin
  */
-public class FileSerializable {
+public interface FileSerializable {
     String toFileString();
 }
