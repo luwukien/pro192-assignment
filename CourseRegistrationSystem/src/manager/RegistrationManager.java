dc5ede71e25e4ae74a2b7d3b6eca4145e1eb1a74
ChiKien_Luu <luukien2910@gmail.com>
2025-10-30 13:14:42 +0700

    feat: adding func registerCourse

--- CourseRegistrationSystem/src/manager/RegistrationManager.java
+++ CourseRegistrationSystem/src/manager/RegistrationManager.java
@@ -1,20 +1,122 @@
 package manager;
 
 import java.util.List;
+import java.util.ArrayList;
 import interfaces.Displayable;
+import data.Student;
 import data.Registration;
-                                                                                                                                                                                                                                                                                                     
+import data.CourseSection;
+import data.Subject;
+import enums.RegistrationStatus;
+
 public class RegistrationManager extends Management<Registration> implements Displayable {
 
     private List<Registration> registrations;
+    private StudentManager studentManager;
+    private CourseManager courseManager;
+    private SubjectManager subjectManager;
 
-    public RegistrationManager(List<Registration> initialList) {
+    public RegistrationManager(List<Registration> initialList, StudentManager studentMan,
+            CourseManager courseMan,
+            SubjectManager subjectMan) {
         super(initialList);
         this.registrations = this.list;
+        this.studentManager = studentMan;
+        this.courseManager = courseMan;
+        this.subjectManager = subjectMan;
     }
-    
+
     public boolean registerCourse(String studentId, String courseSectionId) {
+        Student student = studentManager.findById(studentId);
+        CourseSection course = courseManager.findById(courseSectionId);
+
+        //Input Student and Course check
+        if (student == null || course == null) {
+            return false;
+        }
+
+        //The numbers of course cCeck
+        if (course.isFull()) {
+            return false;
+        }
+
+        //Duplicate Subjects Check
+        for (Registration registration : registrations) {
+            if (registration.getStudentId().equals(studentId) && registration.getCourseSectionId().equals(courseSectionId)) {
+                System.out.println("Please enter other student ID or courseSection ID. Because this student registrated this course!");
+                return false;
+            }
+        }
+
+        //Credit Limit Check
+        String subjectId = course.getSubjectId();
+        Subject subject = subjectManager.findById(subjectId);
+        int newCredits = subject.getCredit();
+        int targetSemester = course.getSemester();
+        int currentCredits = calculateCurrentCredits(studentId, targetSemester);
+        if (currentCredits + newCredits > 20) {
+            System.out.println("Error: Over 20 credits. A student can only register not exceeding 20 credits in a semester!");
+            return false;
+        }
+
+        //Prerequisite Check
+        List<String> prerequisiteSubjects = subject.getPrerequisiteSubjectIds();
+        List<Registration> studentRegistrations = this.getRegistrationsByStudent(studentId);
+
+        for (String preSubjectId : prerequisiteSubjects) {
+            boolean foundAndPassed = false;
+
+            for (Registration registration : studentRegistrations) {
+                CourseSection historicalSection = courseManager.findById(registration.getCourseSectionId());
+                if (historicalSection == null) {
+                    continue;
+                }
+                String historicalSubjectId = historicalSection.getSubjectId();
+                if (historicalSubjectId.equals(preSubjectId)) {
+                    if (registration.getStatus() == RegistrationStatus.PASSED) {
+                        foundAndPassed = true;
+                        break;
+                    }
+                }
+            }
+            if (!foundAndPassed) {
+                System.out.println("Error: The id student: " + studentId + " not pass prerequisite subject id: " + preSubjectId + "!");
+                return false;
+            }
+        }
         
+        Registration newRegis = new Registration(studentId, courseSectionId, 0.0, RegistrationStatus.ENROLLED);
+        super.add(newRegis);
+        course.incrementStudentCount();
+        courseManager.update(course);
+        System.out.println("Student ID: " + studentId + " registered this course section successfully");
+        return true;
+    }
+
+    private int calculateCurrentCredits(String studentId, int semester) {
+        int totalCredits = 0;
+
+        for (Registration registration : getRegistrationsByStudent(studentId)) {
+            if (registration.getStatus() == RegistrationStatus.ENROLLED) {
+                CourseSection section = courseManager.findById(registration.getCourseSectionId());
+                if (section.getSemester() == semester) {
+                    Subject subject = subjectManager.findById(section.getSubjectId());
+                    totalCredits += subject.getCredit();
+                }
+            }
+        }
+        return totalCredits;
+    }
+
+    public List<Registration> getRegistrationsByStudent(String studentId) {
+        ArrayList<Registration> resultList = new ArrayList<>();
+
+        for (Registration registration : registrations) {
+            if (registration.getStudentId().equals(studentId)) {
+                resultList.add(registration);
+            }
+        }
+        return resultList;
     }
 
     @Override
