60f760b8d8bc2a5a91ee68f6ffbcc1c2d4faf127
ChiKien_Luu <luukien2910@gmail.com>
2025-11-02 20:22:44 +0700

    fix: some bugs

--- CourseRegistrationSystem/src/manager/CourseManager.java
+++ CourseRegistrationSystem/src/manager/CourseManager.java
@@ -1,25 +1,33 @@
 package manager;
 
 import java.util.List;
+import java.util.ArrayList;
 import interfaces.Displayable;
 import data.CourseSection;
 
 public class CourseManager extends Management<CourseSection> implements Displayable {
 
-    private List<CourseSection> courseSections;
-
     public CourseManager(List<CourseSection> initialList) {
         super(initialList);
-        this.courseSections = this.list;
+    }
+
+    public List<CourseSection> getSectionsBySubject(String subjectId) {
+        ArrayList<CourseSection> resultList = new ArrayList<>();
+        for (CourseSection section : this.list) {
+            if (section.getSubjectId().equals(subjectId)) {
+                resultList.add(section);
+            }
+        }
+        return resultList;
     }
 
     @Override
     public void displayAll() {
-        if (this.courseSections.isEmpty()) {
+        if (this.list.isEmpty()) {
             System.out.println("Empty list");
             return;
         }
-        for (CourseSection course : courseSections) {
+        for (CourseSection course : this.list) {
             System.out.println(course);
         }
     }
